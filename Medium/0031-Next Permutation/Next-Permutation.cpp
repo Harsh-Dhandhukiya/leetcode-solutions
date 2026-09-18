55907a0db650c57e45cf5977e69c311074154cbe
Harsh-Dhandhukiya <25harsh012005@gmail.com>
2026-09-18 10:24:05 +0530

    Solved: 31. Next Permutation

---
+++ Medium/0031-Next Permutation/Next-Permutation.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    void nextPermutation(std::vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+        
+        // Step 1: Find the first decreasing element from the end
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        
+        if (i >= 0) {
+            // Step 2: Find the element just larger than nums[i] from the end
+            int j = n - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            // Step 3: Swap them
+            std::swap(nums[i], nums[j]);
+        }
+        
+        // Step 4: Reverse the suffix
+        std::reverse(nums.begin() + i + 1, nums.end());
+    }
+};
